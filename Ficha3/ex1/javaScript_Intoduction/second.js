141206b53087a25bcfcc4db03aff4600872c7436
Furia4 <isabel.6.066@gmail.com>
2018-03-18 17:59:59 +0000

    New

--- Ficha3/ex1/javaScript_Intoduction/second.js
+++ Ficha3/ex1/javaScript_Intoduction/second.js
@@ -18,3 +18,29 @@ console.log(price);
 let changeMe=true;
 changeMe=false;
 console.log(changeMe);
+
+/*  ---   */
+let notDefined;
+let valueless;
+valueless = undefined;
+/*  ---  */
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+molecule +=16;
+
+// Multiply and assign to particle below
+particle *= 6.02;
+
+// Increment assay by 1
+assay ++;
+/*  ----Print variable----  */
+let favoriteAnimal='Pug';
+console.log('My favorite animal: '+favoriteAnimal);
+/*      --- */
+let myName = 'Isabel';
+let myCity = 'Felgueiras';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
