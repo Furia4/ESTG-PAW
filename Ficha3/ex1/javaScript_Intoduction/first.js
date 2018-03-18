c65f75ff779b1d908e472a67f886801d2126087d
Furia4 <isabel.6.066@gmail.com>
2018-03-18 14:51:16 +0000

    Start tutorial

---
+++ Ficha3/ex1/javaScript_Intoduction/first.js
@@ -0,0 +1,12 @@
+console.log("I like to read divergente");
+console.log(65 / 0.25);
+console.log(2*4);
+console.log(2+3);
+console.log(2-1);
+console.log('Teaching the world how to code'.length);
+console.log('Codecademy'.toUpperCase()); //print em maiusculas
+console.log('    Remove whitespace   '.trim()); //remove espacos
+console.log(Math.random()*100);
+console.log(Math.floor(Math.random()*100));
+console.log(Math.floor(Math.ceil(43.8)));
+console.log(Number.isInteger(2017));
