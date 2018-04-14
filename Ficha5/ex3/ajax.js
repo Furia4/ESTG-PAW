381464986b8aee8ae274d512469513232165ced1
Furia4 <isabel.6.066@gmail.com>
2018-04-14 15:57:04 +0100

    2 tutorial

---
+++ Ficha5/ex3/ajax.js
@@ -0,0 +1,162 @@
+/* GET*/
+$.ajax({
+  url: 'https://api-to-call.com/endpoint', 
+  type: 'GET',
+  dataType: 'json', 
+  success(response) {
+    console.log(response);
+  }, error(jqXHR, status, errorThrown) {
+    console.log(jqXHR);
+  }
+});
+
+/* post */
+$.ajax({
+  url: 'https://api-to-call.com/endpoint',
+  type: 'POST',
+  data: JSON.stringify({id: 200}), 
+  dataType: 'json', 
+  success(response) {
+    console.log(response);
+  }, error(jqXHR, status, errorThrown) {
+    console.log(jqXHR);
+  }
+});
+
+
+/*
+// Include data for accessing Google APIs
+
+const apiKey = '<Your API Key>';
+const url = 'https://www.googleapis.com/urlshortener/v1/url';
+
+// Some page elements
+
+const $inputField = $('#input');
+const $expandButton = $('#expand');
+const $shortenButton = $('#shorten');
+const $responseField = $('#responseField');
+
+// AJAX functions
+
+function expandUrl() {
+  const urlToExpand = url + 
+        '?key=' + apiKey +
+        '&shortUrl=' + $inputField.val();
+  $.ajax({
+    url: urlToExpand, 
+    type: 'GET',
+    dataType: 'json', 
+    success(response) {
+      $responseField.append('<p>Your expanded url is: </p><p>' + response.longUrl + '</p>');
+    }, error(jqXHR, status, errorThrown) {
+      console.log(jqXHR);
+    }
+  });
+}
+
+function shortenUrl() {
+  const urlWithKey = url + '?key=' + apiKey;
+  const urlToShorten = $inputField.val();
+  const data = JSON.stringify({longUrl: urlToShorten});
+
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      console.log(xhr.response);
+      $responseField.append('<p>Your shortened url is: </p><p>' + xhr.response.id + '</p>');
+    }
+  };
+
+  xhr.open('POST', urlWithKey);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(data);
+}
+
+function expand() {
+  $responseField.empty();
+  expandUrl();
+  return false;
+}
+
+function shorten() {
+  $responseField.empty();
+  shortenUrl();
+  return false;
+}
+
+// Call functions on submit
+
+$expandButton.click(expand);
+$shortenButton.click(shorten);
+
+*//*
+// Include data for accessing Google APIs
+
+const apiKey = '<Your API Key>';
+const url = 'https://www.googleapis.com/urlshortener/v1/url';
+
+// Some page elements
+
+const $inputField = $('#input');
+const $expandButton = $('#expand');
+const $shortenButton = $('#shorten');
+const $responseField = $('#responseField');
+
+// AJAX functions
+
+function expandUrl() {
+  const urlToExpand = url + 
+        '?key=' + apiKey +
+        '&shortUrl=' + $inputField.val();
+  $.ajax({
+    url: urlToExpand, 
+    type: 'GET',
+    dataType: 'json', 
+    success(response) {
+      $responseField.append('<p>Your expanded url is: </p><p>' + response.longUrl + '</p>');
+    },
+    error(jqXHR, status, errorThrown) {
+      console.log(jqXHR);
+    }
+  });
+};
+
+function shortenUrl() {
+  const urlWithKey = url + '?key=' + apiKey;
+  const urlToShorten = $inputField.val();
+
+  $.ajax({
+    url: urlWithKey, 
+    type: 'POST', 
+    data: JSON.stringify({longUrl: urlToShorten}), 
+    dataType: 'json', 
+    contentType: 'application/json', 
+    success(response) {
+      $responseField.append('<p>Your shortened url is: </p><p>' + response.id + '</p>');
+    },
+    error(jqXHR, status, errorThrown) {
+      console.log(jqXHR);
+    }
+  });
+}
+
+function expand() {
+  $responseField.empty();
+  expandUrl();
+  return false;
+}
+
+function shorten() {
+  $responseField.empty();
+  shortenUrl();
+  return false;
+}
+
+// Call functions on submit
+
+$expandButton.click(expand);
+$shortenButton.click(shorten);
+
+*/
